5395d2450abd30efc0bb83303f6f64c661d2bcf7
Camilo Camacho <cama0047@algonquinlive.com>
2021-03-16 11:43:13 -0400

    Add project files.

---
+++ UWPFinal-Camacho/MainPage.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace UWPFinal_Camacho
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void ImageButton_Click(object sender, RoutedEventArgs e)
+        {
+
+            //navigation to the image page - Navegar a la pagina de imagenes 
+            Frame.Navigate(typeof(ImagePage));
+            
+        }
+
+        private void videoButton_Click(object sender, RoutedEventArgs e)
+        {
+
+            //navigation to the video page - Navegar a la pagina de video 
+            Frame.Navigate(typeof(VideoPage));
+            
+        }
+
+        private void aboutButton_Click(object sender, RoutedEventArgs e)
+        {
+            //navigation to the about page - Navegar a la pagina de about 
+            Frame.Navigate(typeof(AboutPage));
+        }
+    }
+}
---
+++ UWPFinal-Camacho/VideoPage.xaml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace UWPFinal_Camacho
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class VideoPage : Page
+    {
+        public VideoPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void imageButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void videoButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+
+        #region Navigation To and From with Back Button
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            var currentView = Windows.UI.Core.SystemNavigationManager.GetForCurrentView();
+
+            currentView.AppViewBackButtonVisibility = Windows.UI.Core.AppViewBackButtonVisibility.Visible;
+
+            currentView.BackRequested += backButton_Tapped; //back -atras
+
+            base.OnNavigatedTo(e);
+        }
+
+        private void backButton_Tapped(object sender, Windows.UI.Core.BackRequestedEventArgs e)
+        {
+            if (Frame.CanGoBack)
+            {
+                Frame.GoBack();
+                e.Handled = true; // tells the OS we handled the back button event (must add so we can also support devices with physical/hardware back buttons)
+            }
+        }
+
+        protected override void OnNavigatedFrom(NavigationEventArgs e)
+        {
+            var currentView = Windows.UI.Core.SystemNavigationManager.GetForCurrentView();
+
+            currentView.AppViewBackButtonVisibility = Windows.UI.Core.AppViewBackButtonVisibility.Collapsed;
+
+            currentView.BackRequested -= backButton_Tapped;
+
+            base.OnNavigatedFrom(e);
+        }
+
+        #endregion
+
+    }
+}
